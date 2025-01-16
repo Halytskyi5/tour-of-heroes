2527df595eae12097e2371a11c452b7711bb254f
Halytskyi5 <volodyaforschool@gmail.com>
2025-01-16 17:51:41 +0200

    before dashboard component

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { HeroesComponent} from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   standalone: true, // !!!!!
   selector: 'app-root',
   imports:[
     HeroesComponent,
-    MessagesComponent
+    MessagesComponent,
+    RouterOutlet,
+    RouterLink
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- angular-tour-of-heroes/src/app/app.routes.ts
+++ angular-tour-of-heroes/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import {AppComponent} from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  /*{
+    path: '',
+    title: 'Home page',
+    component: AppComponent
+  },*/
+  {
+    path: 'heroes',
+    title: 'All heroes',
+    component: HeroesComponent
+  }
+];
